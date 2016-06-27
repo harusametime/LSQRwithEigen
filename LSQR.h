d1de34cb10fc4b47a94c3a8e53cd3a3bbfd0be71
Masaki Samejima <samejima@ist.osaka-u.ac.jp>
2016-06-27 09:37:11 +0900

    Create LSQR.h

---
+++ LSQR.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "./Eigen/Eigen/Dense"
+#include "./Eigen/Eigen/Sparse"
+#include <iostream>
+
+
+class LSQR
+{
+public:
+	Eigen::VectorXd x;
+	LSQR(const Eigen::SparseMatrix<double> &A, const Eigen::VectorXd &b, const Eigen::VectorXd &x_ini, const double &gamma);
+	Eigen::VectorXd SolutionX();
+};
+
